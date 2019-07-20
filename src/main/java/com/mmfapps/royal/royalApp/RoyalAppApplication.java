993c79dde7a730070fedac9baf0e93cabfc6d99e
Mario Muciño <mario.mucino.flores@ibm.com>
2019-07-20 14:35:27 -0500

    first commit

---
+++ src/main/java/com/mmfapps/royal/royalApp/RoyalAppApplication.java
@@ -0,0 +1,13 @@
+package com.mmfapps.royal.royalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmfapps/royal/royalApp/RoyalAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmfapps.royal.royalApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoyalAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
