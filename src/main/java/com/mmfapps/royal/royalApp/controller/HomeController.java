1a33f0ae696fd0244bee116b2800f236e0dfe61b
Mario Muciño <mario.mucino.flores@ibm.com>
2019-07-22 15:26:41 -0500

    add modifications and maifest

--- src/main/java/com/mmfapps/royal/royalApp/controller/HomeController.java
+++ src/main/java/com/mmfapps/royal/royalApp/controller/HomeController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index() {
 		return "index.html";
 	}
 	@GetMapping("/ScheduleEvent")
 	public String ScheduleEvent() {
-		return "ScheduleEvent.html";
+		return "/pages/ScheduleEvent.html";
 	}
 	@GetMapping("/photographer")
 	public String photographer() {
-		return "photographer.html";
+		return "/pages/photographer.html";
 	}
 	
 
