8611f56e0e6a4f8bfdc47843e409618d41d6d589
Mario Muciño <mario.mucino.flores@ibm.com>
2019-07-22 16:46:44 -0500

    no message

--- src/main/java/com/mmfapps/royal/royalApp/controller/HomeController.java
+++ src/main/java/com/mmfapps/royal/royalApp/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	
 	@GetMapping("/index")
 	public String index() {
-		return "index.html";
+		return "/index.html";
 	}
 	@GetMapping("/ScheduleEvent")
 	public String ScheduleEvent() {
