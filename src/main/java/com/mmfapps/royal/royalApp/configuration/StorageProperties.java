8d8a666fda2ed71200d7c6aace60e1c462caade1
Mario Muciño <mario.mucino.flores@ibm.com>
2019-07-27 18:14:14 -0500

    Add some functionality to upload file

---
+++ src/main/java/com/mmfapps/royal/royalApp/configuration/StorageProperties.java
@@ -0,0 +1,21 @@
+package com.mmfapps.royal.royalApp.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "C:\\Users\\MarioMucinoFlores\\Documents\\Test";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
---
+++ src/main/java/com/mmfapps/royal/royalApp/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mmfapps.royal.royalApp.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
