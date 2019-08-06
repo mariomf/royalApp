fb9d5eeb5223865dac3b13ee42e2c5320b8b0638
Mario Muciño <mario.mucino.flores@ibm.com>
2019-08-05 19:41:04 -0500

    1

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -5,33 +5,42 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
 
-      document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
+      /*document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";*/
     } else {
       // No user is signed in.
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
+      /*document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";*/
     }
   });
 
 function login(){
 
-    var userEmail = $("#email_field").val();
-    var userPass = $("#password_field").val();
-    //var userEmail = document.getElementById("email_field").value;
-    //var userPass = document.getElementById("password_field").value;
+    //var userEmail = $("#email_field").val();
+    //var userPass = $("#password_field").val();
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
 
-    window.alert(userEmail + " " + userPass + "fdsadfkjdsafk");
+    
 
     console.log(userEmail.toString());
     console.log(userPass.toString());
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      window.alert("Error..." + errorMessage);
+      // ...
+    });
+
+    window.alert(userEmail + " " + userPass + "fdsadfkjdsafk");
+    /*firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
 
-        window.alert("Error..")
-        // ...
-      });
+        window.alert("Error.." + errorMessage);
+      });*/
 }
