add61f0f094b5eea8e0abae6a2e0d61efd3ca6b8
Mario Muciño <mario.mucino.flores@ibm.com>
2019-08-21 22:42:27 -0500

    add login and signup

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -6,15 +6,13 @@ firebase.auth().onAuthStateChanged(function(user) {
 
       console.log("Is loged in");
       // User is signed in.
-
+      window.location.replace("/");
       /*document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";*/
     } else {
 
       console.log("Is NOT loged in");
       // No user is signed in.
-      /*document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";*/
     }
   });
 
@@ -24,6 +22,22 @@ firebase.auth().onAuthStateChanged(function(user) {
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
   firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).then(cred => {console.log(cred);}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error..." + errorMessage);
+    // ...
+  });
+
+ })
+
+ const loginForm = document.querySelector('#login-form');
+ loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var userEmail_login = document.getElementById("email_field_login").value;
+  var userPass_login = document.getElementById("password_field_login").value;
+  firebase.auth().signInWithEmailAndPassword(userEmail_login, userPass_login).then(cred => {console.log(cred);}).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
