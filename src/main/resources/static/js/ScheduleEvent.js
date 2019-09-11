fcf67c36310f61e8d9b6bae367de4b0c9de6a42e
Mario M <mario.mucino.flores@gmail.com>
2019-09-11 18:45:01 -0500

    other

--- src/main/resources/static/js/ScheduleEvent.js
+++ src/main/resources/static/js/ScheduleEvent.js
@@ -5,3 +5,13 @@ $(function () {
         sideBySide: true}*/
         );
 });
+// $('#input_starttime').pickatime({
+//   // Light or Dark theme
+//   darktheme: true
+//   // 12 or 24 hour
+//   twelvehour: true
+// });
+
+$(document).ready(function(){
+  $(#input_starttime).timepicker();
+});
