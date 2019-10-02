a3c001c5bd05b5f9b9bf784bb8d70f6ce53718b6
Mario Muciño <mario.mucino.flores@ibm.com>
2019-10-01 21:49:58 -0500

    Get date and time, pass it to the review order

--- src/main/resources/static/js/ScheduleEvent.js
+++ src/main/resources/static/js/ScheduleEvent.js
@@ -1,24 +1,46 @@
-//$(document).ready(
-$(function () {
-    $('#datetimepicker').datetimepicker(
-        /*{inline: true,
-        sideBySide: true}*/
-        );
-});
-// $('#input_starttime').pickatime({
-//   // Light or Dark theme
-//   darktheme: true
-//   // 12 or 24 hour
-//   twelvehour: true
-// });
 
-/*$(document).ready(function(){
-  $(#input_starttime).timepicker();
-});*/
+//$(document).ready(
+
+
+//Date1 = document.getElementsByClassName('picker-switch');
+
+//var aux1 = $('#datetimepicker12').data("DateTimePicker").viewDate();
+//console.log(aux1);
+var aux1;
+
+$(function () {
+  $('#datetimepicker12').datetimepicker({
+    inline: true,
+    sideBySide: true
+  });
+  $('#datetimepicker12').data("DateTimePicker").stepping(15);
+  aux1 = $('#datetimepicker12').data("DateTimePicker").viewDate();
+  var aux3 = $('#datetimepicker12').data("DateTimePicker").viewMode();
+  var aux2 = $('#datetimepicker12').datetimepicker();
+  console.log(aux1);
+  console.log(aux1._d);
+  console.log(aux3);
+  console.log(aux2);
+});
 
 function reviewOrder1HourSocial(){
+  //$('#datetimepicker').data("DateTimePicker").FUNCTION()
+  aux1 = $('#datetimepicker12').data("DateTimePicker").viewDate();
+  localStorage.setItem('DateAndTime', aux1._d);
   localStorage.setItem('ServiceType','1HSocial');
-  
-  location.href="/ReviewOrder?ServiceType=1HSocial";
+  location.href="/ReviewOrder";
 
 }
+//);
+
+
+//********** GET DATE AND TIME **********/
+
+
+
+
+
+
+
+
+
--- src/main/resources/static/js/reviewOrder.js
+++ src/main/resources/static/js/reviewOrder.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
   // Adapted from examples on the Querystring homepage.
   // var qs = new Querystring();
   // var v1 = qs.get("ServiceType");
-var aux = '';
+  var aux = '';
   // Get it from the local storage
   var selected = localStorage.getItem('ServiceType');
+  var selected1 = localStorage.getItem('DateAndTime');
   // var aux ="<h1>"+v1+"</h1>";
   aux += "<h2>"+selected+"</h2>";
   $('.h1la').append(selected);
 
+  console.log(selected);
+  console.log(selected1);
+
 });
+
+function ScheduleEvent() {
+  location.href = "/ScheduleEvent";
+
+}
