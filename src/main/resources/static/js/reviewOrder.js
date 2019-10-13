4a60fbb675b36f17113cc7e3befeb43f9149053c
Mario Muciño <mario.mucino.flores@ibm.com>
2019-10-13 14:44:17 -0500

    Set total price

--- src/main/resources/static/js/reviewOrder.js
+++ src/main/resources/static/js/reviewOrder.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
   // Adapted from examples on the Querystring homepage.
   // var qs = new Querystring();
   // var v1 = qs.get("ServiceType");
-  var aux = '';
+  //var aux = '';
   // Get it from the local storage
   var selected = localStorage.getItem('ServiceType');
   var selected1 = localStorage.getItem('DateAndTime');
+  var TotalPrice = 0;
   //var aux1 = "<small class='text-muted'>"+selected1+"</small>";
-  aux += "<h2>"+selected+"</h2>";
-  $('.h1la').append(selected);
+  //aux += "<h2>"+selected+"</h2>";
+  //$('.h1la').append(selected);
 //  $('.SelectedDate').append(aux1)
   document.getElementById('SelectedDate').innerHTML = selected1;
   console.log("selected="+selected);
@@ -21,6 +22,8 @@ $(document).ready(function(){
     console.log("inside 1HSocial");
     document.getElementById('SelectedHours').innerHTML = "01:00 hora";
     document.getElementById('PriceHours').innerHTML = "$600";
+
+    TotalPrice = 600;
   }
 
   //2 HOUR OF ROYAL SOCIAL
@@ -28,6 +31,8 @@ $(document).ready(function(){
     console.log("inside 2HSocial");
     document.getElementById('SelectedHours').innerHTML = "02:00 hora";
     document.getElementById('PriceHours').innerHTML = "$1,100";
+
+    TotalPrice = 1100;
   }
 
   //3 HOUR OF ROYAL SOCIAL
@@ -35,12 +40,16 @@ $(document).ready(function(){
     console.log("inside 3HSocial");
     document.getElementById('SelectedHours').innerHTML = "03:00 hora";
     document.getElementById('PriceHours').innerHTML = "$1,500";
+
+    TotalPrice = 1500;
   }
 
   //MORE HOURS OF ROYAL SOCIAL
   if (selected == "MoreHSocial") {
     document.getElementById('SelectedHours').innerHTML = "01:00 hora";
     document.getElementById('PriceHours').innerHTML = "$600";
+
+    TotalPrice = 600;
   }
 /********************* ROYAL SOCIAL OPTIONS **********************************/
 
@@ -50,26 +59,37 @@ $(document).ready(function(){
   if (selected == "1HBusiness") {
     document.getElementById('SelectedHours').innerHTML = "01:00 hora";
     document.getElementById('PriceHours').innerHTML = "$600";
+
+    TotalPrice = 600;
   }
 
   //2 HOUR OF ROYAL BUSINESS
   if (selected == "2HBusiness") {
     document.getElementById('SelectedHours').innerHTML = "02:00 hora";
     document.getElementById('PriceHours').innerHTML = "$1,100";
+
+    TotalPrice = 1100;
   }
 
   //3 HOUR OF ROYAL BUSINESS
   if (selected == "3HBusiness") {
     document.getElementById('SelectedHours').innerHTML = "03:00 hora";
     document.getElementById('PriceHours').innerHTML = "$1,500";
+
+    TotalPrice = 1500;
   }
 
   //MORE HOURS OF ROYAL BUSINESS
   if (selected == "MoreHBusiness") {
     document.getElementById('SelectedHours').innerHTML = "01:00 hora";
     document.getElementById('PriceHours').innerHTML = "$600";
+
+    TotalPrice = 600;
   }
 /********************* ROYAL BUSINESS OPTIONS **********************************/
+
+document.getElementById('TotalPrices').innerHTML = "$" + TotalPrice;
+
 });
 
 /************* HREFs ********************************/
